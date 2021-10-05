9daf3b69ada1d2963977194f90821daebd1feac3
Mansur <oralov.mansur@gmail.com>
2021-10-05 04:21:13 -0700

    Have added a new class for the secretKey generation. Now the application generates the secret keys for asymmetric encryption automatically without it being hardcoded.

--- src/mallochite/encryption/EncryptionMain.java
+++ src/mallochite/encryption/EncryptionMain.java
@@ -1,13 +1,17 @@
 package mallochite.encryption;
 
+import java.security.NoSuchAlgorithmException;
 
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
 
 public class EncryptionMain {
 	
-	static String secretKey = "John";
-	
+public static String secretKey;
+
+
 	public static String encrypt(String message) throws Exception {
-		
+		secretKey = SecretKeyGenerator.GenerateKey();
 		message = AESEncryption.encrypt(message, secretKey);
 		secretKey = RSAEncryption.rsaEncrypt(secretKey);	
 		
